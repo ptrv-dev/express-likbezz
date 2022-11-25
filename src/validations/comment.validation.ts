74fe7b139d8c00842e1832b3b7e40e1fbc505b72
ptrv-dev <petrov99119@gmail.com>
2022-11-25 14:21:08 +0200

    Comment create validation

---
+++ src/validations/comment.validation.ts
@@ -0,0 +1,6 @@
+import { body } from 'express-validator';
+
+export const commentCreateValidation = [
+  body('post').isString(),
+  body('text').isLength({ min: 2, max: 512 }),
+];
--- src/validations/post.validation.ts
+++ src/validations/post.validation.ts
@@ -3,5 +3,5 @@ import { body } from 'express-validator';
 export const postCreateValidation = [
   body('image').optional().isString(),
   body('title').isLength({ min: 4, max: 128 }),
-  body('text').isLength({ min: 10, max: 1024 }),
+  body('text').isLength({ min: 10, max: 4096 }),
 ];
