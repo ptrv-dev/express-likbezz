1d77f87cb57dc18e016d76b68d23fdaf136ba252
ptrv-dev <petrov99119@gmail.com>
2022-11-30 13:16:32 +0200

    again cors fix

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ export const upload = multer({ storage });
 
 // Express config
 const app = express();
-app.use(cors({ origin: false, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
